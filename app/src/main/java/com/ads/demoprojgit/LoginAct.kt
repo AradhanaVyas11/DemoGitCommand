4e36574108b20f1b652dfc81cb2322c1113a6259
Aradhana Vyas <aradhana.vyas@auronia.io>
2022-02-14 10:44:26 +0530

    Add Login

--- app/src/main/java/com/ads/demoprojgit/LoginAct.kt
+++ app/src/main/java/com/ads/demoprojgit/LoginAct.kt
@@ -1,12 +1,38 @@
 package com.ads.demoprojgit
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginAct : AppCompatActivity(){
+class LoginAct : AppCompatActivity(), View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        tvRegister.setOnClickListener(this)
+        tvForgotPassword.setOnClickListener(this)
+        btnSignIn.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        val itemId = view?.id
+        when (itemId) {
+            R.id.tvRegister -> {
+                val intent = Intent(this, RegistrationAct::class.java)
+                startActivity(intent)
+            }
+
+            R.id.tvForgotPassword -> {
+                Log.e("TAG", "Forgot Password")
+            }
+
+            R.id.btnSignIn -> {
+                Log.e("TAG", "Sign In")
+            }
+        }
     }
 }
